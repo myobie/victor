7c1907a93519e8d932dcb6b5415b92e257a94404
Nathan Herald <me@nathanherald.com>
2018-02-22 14:14:00 +0100

    Brunch is driving me crazy

--- assets/brunch-config.js
+++ assets/brunch-config.js
@@ -1,62 +1,34 @@
-exports.config = {
-  // See http://brunch.io/#documentation for docs.
-  files: {
-    javascripts: {
-      joinTo: "js/app.js"
-
-      // To use a separate vendor.js bundle, specify two files path
-      // http://brunch.io/docs/config#-files-
-      // joinTo: {
-      //   "js/app.js": /^js/,
-      //   "js/vendor.js": /^(?!js)/
-      // }
-      //
-      // To change the order of concatenation of files, explicitly mention here
-      // order: {
-      //   before: [
-      //     "vendor/js/jquery-2.1.1.js",
-      //     "vendor/js/bootstrap.min.js"
-      //   ]
-      // }
-    },
-    stylesheets: {
-      joinTo: "css/app.css"
-    },
-    templates: {
-      joinTo: "js/app.js"
-    }
+exports.files = {
+  javascripts: {
+    joinTo: 'js/app.js'
   },
-
-  conventions: {
-    // This option sets where we should place non-css and non-js assets in.
-    // By default, we set this to "/assets/static". Files in this directory
-    // will be copied to `paths.public`, which is "priv/static" by default.
-    assets: /^(static)/
-  },
-
-  // Phoenix paths configuration
-  paths: {
-    // Dependencies and current project directories to watch
-    watched: ["static", "css", "js", "vendor"],
-    // Where to compile files to
-    public: "../priv/static"
-  },
-
-  // Configure your plugins
-  plugins: {
-    babel: {
-      preset: ["env"],
-      ignore: [/vendor/]
-    }
-  },
-
-  modules: {
-    autoRequire: {
-      "js/app.js": ["js/app"]
-    }
-  },
-
-  npm: {
-    enabled: true
+  stylesheets: {
+    joinTo: 'css/app.css'
   }
-};
+}
+
+exports.conventions = {
+  assets: /^(static)/
+}
+
+exports.paths = {
+  watched: ['static', 'css', 'js', 'vendor'],
+  public: '../priv/static'
+}
+
+exports.plugins = {
+  babel: {
+    presets: ['@babel/preset-env'],
+    ignore: [/vendor/]
+  }
+}
+
+exports.modules = {
+  autoRequire: {
+    'js/app.js': ['js/app']
+  }
+}
+
+exports.npm = {
+  enabled: true
+}
